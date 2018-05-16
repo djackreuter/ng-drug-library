8c04420ac119bd7ccc1da079c448a788fca9099f
Jack Reuter <djreuter45@gmail.com>
2018-05-16 14:47:58 -0600

    added drug service

---
+++ src/app/services/drug/drug.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class DrugService {
+
+  constructor(private http: HttpClient) { }
+
+  drugUrl = 'http://localhost:3000/drugs/query?search=';
+
+  getDrugs(searchQuery): Observable<object> {
+    return this.http.get(this.drugUrl + searchQuery);
+  }
+
+}
