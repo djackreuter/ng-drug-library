8634da945cc946d67ff444b57eba14b2510aa19a
Jack Reuter <djreuter45@gmail.com>
2018-05-17 13:12:16 -0600

    added input decorator to drug property

--- src/app/components/drug-details/drug-details.component.ts
+++ src/app/components/drug-details/drug-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-drug-details',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drug-details.component.css']
 })
 export class DrugDetailsComponent implements OnInit {
+
+  @Input() drug: object;
 
   constructor() { }
 
