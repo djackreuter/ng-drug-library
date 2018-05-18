0ccb74c9088fdcc106024484ab65e28f3e494488
Jack Reuter <djreuter45@gmail.com>
2018-05-18 12:11:11 -0600

    imported angular routing modules

--- src/app/components/drug-details/drug-details.component.ts
+++ src/app/components/drug-details/drug-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { DrugService } from '../../services/drug/drug.service';
 
 
 @Component({
@@ -8,16 +11,27 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class DrugDetailsComponent implements OnInit {
 
-  @Input() closeModal: any;
-  @Input() drug: object;
+  // @Input() closeModal: any;
+  // @Input() drug: object;
+  drugs: object;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private location: Location,
+              private drugService: DrugService) { }
 
   ngOnInit() {
+    this.getDrug();
   }
 
-  modalClose(): void {
-    this.closeModal.hide()
+  // modalClose(): void {
+  //   this.closeModal.hide()
+  // }
+
+  getDrug(): void {
+    const name = this.route.snapshot.paramMap.get('name');
+    this.drugService.getDrug(name).subscribe((res) => {
+      this.drugs = res;
+    });
   }
 
 }
