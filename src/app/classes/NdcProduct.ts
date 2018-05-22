97a6110c613142f15c77c4e2bf21189e8c7ee14d
Jack Reuter <djreuter45@gmail.com>
2018-05-22 15:30:52 -0600

    added ndc product class

---
+++ src/app/classes/NdcProduct.ts
@@ -0,0 +1,23 @@
+export class NdcProduct {
+  id: number;
+  product_id: string;
+  product_ndc: string;
+  product_type_name: string;
+  proprietary_name: string;
+  proprietary_name_suffix: string;
+  non_proprietary_name: string;
+  dosage_form_name: string;
+  route_name: string;
+  start_marketing_date: string;
+  end_marketing_date: string;
+  marketing_category_name: string;
+  application_number: string;
+  labeler_name: string;
+  substance_name: string;
+  active_numerator_strength: string;
+  active_ingred_unit: string;
+  pharm_classes: string;
+  dea_schedule: string;
+  ndc_exclude_flag: string;
+  listing_record_certified_through: string;
+}
