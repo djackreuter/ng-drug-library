2e3b736b7c72c2ca233def7b3bf9bc9eac4d9219
Jack Reuter <djreuter45@gmail.com>
2018-05-23 13:18:26 -0600

    imported reactive forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppComponent } from './app.component';
@@ -23,7 +23,8 @@ import { AppRoutingModule } from './/app-routing.module';
     HttpClientModule,
     FormsModule,
     ModalModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [
     DrugService
