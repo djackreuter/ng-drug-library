9ca2c6f2c2df225ad4a870e455c7c208d4182614
Jack Reuter <djreuter45@gmail.com>
2018-05-23 13:18:46 -0600

    added prescription data class

---
+++ src/app/classes/prescriptionData.ts
@@ -0,0 +1,18 @@
+export class PrescriptionData {
+  directions: string;
+  patientNotes: string;
+  quantityOfMed: string;
+}
+
+export const quantityToTake: string[] = ['1', '2', '3'];
+export const frequency: string[] = ['Once per day', 'Twice per day', 'Three times per day', 'As needed'];
+export const timeOfDay: string[] = ['Morning', 'Afternoon', 'Evening', 'Before bed'];
+export const durationToTake: string[] = ['10 days', '14 days', '30 days', 'As needed'];
+export const refills: string[] = ['1', '2', '3', 'None'];
+
+
+
+
+
+
+
