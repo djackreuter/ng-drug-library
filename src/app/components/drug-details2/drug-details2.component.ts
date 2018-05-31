75b1dafdf3c1d72bddc0f749b8b24659ae978622
Jack Reuter <djreuter45@gmail.com>
2018-05-31 10:43:11 -0600

    added drug details selector

--- src/app/components/drug-details2/drug-details2.component.ts
+++ src/app/components/drug-details2/drug-details2.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-drug-details2',
   templateUrl: './drug-details2.component.html',
-  styleUrls: ['./drug-details2.component.css']
+  styleUrls: ['../drug-details/drug-details.component.css']
 })
 export class DrugDetails2Component implements OnInit {
 
