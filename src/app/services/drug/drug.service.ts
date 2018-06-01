882dfe93da5be396efe9eece71a0e0c63c020d3c
Jack Reuter <djreuter45@gmail.com>
2018-06-01 10:41:58 -0600

    added new service method to get specific drug results

--- src/app/services/drug/drug.service.ts
+++ src/app/services/drug/drug.service.ts
@@ -27,4 +27,8 @@ export class DrugService {
     return this.http.get<NdcProduct>(`http://localhost:3000/drugs/strength?search=${drug}&dose_form=${doseForm}`);
   }
 
+  getDrugInfo(drugName: string): Observable<object> {
+    return this.http.get<object>(`http://localhost:3000/drugs/drug_info?search=${drugName}`);
+  }
+
 }
