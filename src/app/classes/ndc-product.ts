0a2f31d5c24407f230f6a50c207c4062c0cc1d34
Jack Reuter <djreuter45@gmail.com>
2018-06-04 16:26:11 -0600

    modified classes

--- src/app/classes/ndc-product.ts
+++ src/app/classes/ndc-product.ts
@@ -1,23 +1,25 @@
 export class NdcProduct {
-  id: number;
-  product_id: string;
-  product_ndc: string;
-  product_type_name: string;
-  proprietary_name: string;
-  proprietary_name_suffix: string;
-  non_proprietary_name: string;
-  dosage_form_name: string;
-  route_name: string;
-  start_marketing_date: string;
-  end_marketing_date: string;
-  marketing_category_name: string;
-  application_number: string;
-  labeler_name: string;
-  substance_name: string;
-  active_numerator_strength: string;
-  active_ingred_unit: string;
-  pharm_classes: string;
-  dea_schedule: string;
-  ndc_exclude_flag: string;
-  listing_record_certified_through: string;
+  constructor(
+    public id: number,
+    public product_id: string,
+    public product_ndc: string,
+    public product_type_name: string,
+    public proprietary_name: string,
+    public proprietary_name_suffix: string,
+    public non_proprietary_name: string,
+    public dosage_form_name: string,
+    public route_name: string,
+    public start_marketing_date: string,
+    public end_marketing_date: string,
+    public marketing_category_name: string,
+    public application_number: string,
+    public labeler_name: string,
+    public substance_name: string,
+    public active_numerator_strength: string,
+    public active_ingred_unit: string,
+    public pharm_classes: string,
+    public dea_schedule: string,
+    public ndc_exclude_flag: string,
+    public listing_record_certified_through: string
+  ) {}
 }
--- src/app/classes/prescription-form-data.ts
+++ src/app/classes/prescription-form-data.ts
@@ -1,6 +1,8 @@
 export class PrescriptionFormData {
   constructor(
     public quantityToTake: number,
+    public doseForm: string,
+    public strength: string,
     public frequency: number,
     public timeToTake: string,
     public duration: string, 
