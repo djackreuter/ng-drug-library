a44d1e1de3cc668d1bbe3d8b0b7e17977de42428
Jack Reuter <djreuter45@gmail.com>
2018-06-05 10:09:29 -0600

    removed old method

--- src/app/services/drug/drug.service.ts
+++ src/app/services/drug/drug.service.ts
@@ -20,10 +20,6 @@ export class DrugService {
     return this.http.get<NdcProduct[]>(`http://localhost:3000/drugs/query_distinct?search=${drugName}`);
   }
 
-  // getDrug(name: string): Observable<NdcProduct[]> {
-  //   return this.http.get<NdcProduct[]>(this.drugInfoUrl + name);
-  // }
-
   getDosage(drug: string, doseForm: string): Observable<NdcProduct> {
     return this.http.get<NdcProduct>(`http://localhost:3000/drugs/strength?search=${drug}&dose_form=${doseForm}`);
   }
